8c41f092d9d78327e7795de9df7ebeda42fc862c
IvanBojic <ivan.bojic95@gmail.com>
2024-02-11 20:06:44 +0100

    Footer - second design

--- footer.php
+++ footer.php
@@ -4,31 +4,38 @@
         <div class="lg-margin-60px-top md-margin-50px-top sm-padding-40px-top sm-margin-40px-top">
             <div class="row align-items-center">
                 <!-- start logo -->
-                <div class="col-lg-3 col-md-12 text-center text-md-center md-margin-50px-bottom sm-margin-30px-bottom">
+                <div class="col-lg-12 col-md-12 text-center text-md-center md-margin-50px-bottom sm-margin-30px-bottom">
                     <a href="index.php"><img class="footer-logo" src="images/logo.png" data-at2x="images/logo.png" alt="VBS Bezbednost"></a>
                 </div>
                 <!-- end logo -->
                 <!-- start copyright -->
-                <div class="col-lg-3 col-md-5 col-12 sm-margin-30px-bottom text-medium">
+                <a class="flex justify-content-center companyname">Valjaonica Bezbednost d.o.o.</a>
+                <div class="col-lg-12 col-md-5 col-12 sm-margin-30px-bottom footer-text text-medium">
                     Pored usluga privatnog obezbeđenja, naša kompanija pruža i usluge u oblasti smanjenja rizika od katastrofa, kao i ostale usluge u skladu sa zahtevima tržišta i potrebama klijenata
                 </div>
-                <div class="col-lg-3 col-md-4 col-12 sm-margin-30px-bottom text-medium">
-                    <a>Valjaonica Bezbednost d.o.o.</a><br>
-                    <i class="fa fa-map-marker" aria-hidden="true"></i> <a>Prvomajska bb, 31205 Sevojno.</a><br>
-                    <i class="fa fa-phone" aria-hidden="true"></i> Tel: <a href="tel:+38131532203">+381 31 532 203</a><br>
-                    <i class="fa fa-phone" aria-hidden="true"></i> Mob: <a href="tel:+381668684435">+381 66 868 4435</a><br>
-                    <i class="fa fa-envelope" aria-hidden="true"></i> Email: <a href="mailto:valjaonicabezbednost@vbs.co.rs">valjaonicabezbednost@vbs.co.rs</a>
+                <div class="col-lg-12 col-md-4 col-12 sm-margin-30px-bottom text-medium">
+
+                    <div class="flex centerinfo">
+                        <div> <i class="fa fa-map-marker" aria-hidden="true"></i> <a class="footerlink">Prvomajska bb, 31205 Sevojno</a><br></div>
+                        <div> <i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:+38131532203" class="footerlink">+381 31 532 203</a><br></div>
+                        <div><i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:+381668684435" class="footerlink">+381 66 868 4435</a><br></div>
+                        <div><i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:valjaonicabezbednost@vbs.co.rs" class="footerlink">valjaonicabezbednost@vbs.co.rs</a></div>
+                    </div>
                 </div>
                 <!-- end copyright -->
                 <!-- start social media -->
-                <div class="col-lg-3 col-md-3 text-center text-md-center">
-                    <div class="social-icon-style-8 d-inline-block align-middle">
-                        <ul class="medium-icon mb-0">
-                            <li><a class="google text-white-2" href="mailto:pmilutinovic@vbs.co.rs" target="_blank"><i class="fab fa-google-plus-g"></i></a></li>
-                        </ul>
+                <div class="col-lg-12 col-md-3 text-center text-md-center">
+                    <div class="social-icon-style-8 d-inline-block align-middle flex justify-content-center">
+                       <a class="google footergoogle" href="mailto:pmilutinovic@vbs.co.rs" target="_blank"><i class="fab fa-google-plus-g"></i></a>
+
                     </div>
                 </div>
                 <!-- end social media -->
+                <p class="copyright">
+                    &copy; bojovilinno.com | Sva prava zadržana.
+
+                </p>
+                <p class="poweredby">Powered by Ivan Bojic legendica</p>
             </div>
         </div>
     </div>
